2380e7b4bc55a0b19f0a8d8f55cd13efd3ba242c
Akshay Kadu <akshay@rozainfotech.com>
2020-04-22 00:01:13 +0530

    adding router switch for multiple pages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Header from './component/common/Header';
 import Footer from './component/common/Footer';
+import Login from './component/Login/Login';
+import Cashback from './component/Cashback/Cashback';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Curators from './component/Curators/Curators';
 
 function App() {
   return (
-    <Container>
-      <Header/>
-      <Footer/>
-    </Container>
+    <Router>
+      <Container>
+        <Header />
+        <Switch>
+          <Route path="/cashback">
+            <Cashback />
+          </Route>
+          <Route path="/curators">
+            <Curators />
+          </Route>
+        </Switch>
+        <Footer />
+      </Container>
+    </Router>
+
   );
 }
 
--- src/component/common/Header.js
+++ src/component/common/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
@@ -8,9 +9,9 @@ function Header() {
             <Navbar.Brand href="#home">Smyl Dashboard</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav.Link href="#cashback">Cashback</Nav.Link>
-                <Nav.Link href="#cashback">Curators</Nav.Link>
-                <Nav.Link href="#cashback">Payments</Nav.Link>
+                <Nav.Link as={Link} to="/cashback">Cashback</Nav.Link>
+                <Nav.Link as={Link} to="/Curators">Curators</Nav.Link>
+                <Nav.Link as={Link} to="/Payments">Payments</Nav.Link>
             </Navbar.Collapse>
         </Navbar>
     );
